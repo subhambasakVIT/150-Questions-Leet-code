734969ff225c7bdb0407c829905a7fd148600b3b
Subham Basak <137754790+subhambasakVIT@users.noreply.github.com>
2025-08-08 23:25:04 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (66.15%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.java
@@ -0,0 +1,16 @@
+class Solution 
+{
+    public int reverseBits(int n) 
+    {
+        //DAY 51
+        int result = 0;
+        
+        for (int i = 0; i < 32; i++) {
+            result <<= 1;     
+            result |= (n & 1);  
+            n >>>= 1; 
+        }
+        
+        return result;
+    }
+}
