431b0d41322f888eaf00332cfb3609b20b796b96
Subham Basak <137754790+subhambasakVIT@users.noreply.github.com>
2025-08-09 20:22:28 +0530

    Time: 0 ms (100.00%) | Memory: 45.7 MB (41.17%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -2,6 +2,7 @@ class Solution
 {
     public int singleNumber(int[] nums) 
     {
+        //DAY 53
         int ones = 0, twos = 0;
         for (int num : nums) {
             ones = (ones ^ num) & ~twos;
